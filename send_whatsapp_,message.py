bd1c548c08a97afb544021912572f554d2c280e1
HusseinElwakeel <71234180+HusseinElwakeel@users.noreply.github.com>
2023-02-28 23:43:11 +0200

    Add files via upload

---
+++ send_whatsapp_,message.py
@@ -0,0 +1,5 @@
+import pywhatkit as kit
+kit.sendwhatmsg("+201210247381","this is my mobile phone",12,00)
+#mobile want to send the messege
+#content the message
+#the time you want to send the messege
